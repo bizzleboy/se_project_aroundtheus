2e47a1bdbf4c88cc4c403af495dadcf7b472eae1
bizzle <parkerjoe365@gmail.com>
2022-10-20 15:21:06 -0400

    adding arrays

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+let card1 = {
+  name: "Yosemite Valley",
+  link: "../images/yosemite-valley.jpg",
+};
+
+let card2 = {
+  name: "Lake Louise",
+  link: "../images/lake-louise.png",
+};
+
+let card3 = {
+  name: "Bald Mountains",
+  link: "../images/bald-mountains.png",
+};
+
+let card4 = {
+  name: "Latemar",
+  link: "../images/latemar.png",
+};
+
+let card5 = {
+  name: "Vanoise National Park",
+  link: "../images/vanoise-national-park.png",
+};
+
+let card6 = {
+  name: "Yosemite Valley",
+  link: "../images/yosemite-valley.jpg",
+};
+
+initialCards = [card1, card2, card3, card4, card5, card6];
